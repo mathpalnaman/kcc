64a578f04146a1afa20653f8b5f89bd82867c3f0
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2022-09-08 14:15:31 +0530

    Create datatype.cpp

---
+++ datatype.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char ch;
+    int i;
+    long l;
+    double d;
+    float f;
+    cin>>i>>l>>ch>>f>>d;
+    cout<<i<<endl;
+    cout<<l<<endl;
+    cout<<ch<<endl;
+    cout<<f<<endl;
+    cout<<d<<endl;
+    return 0;
+    
+}
